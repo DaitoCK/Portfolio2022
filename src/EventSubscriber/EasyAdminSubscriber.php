1eca33e48ea70de6cd6de906f4254e5e3001a978
DaitoCK <kendhalimax@gmail.com>
2022-02-04 15:11:47 +0100

    fix bug user cannot be null

--- src/EventSubscriber/EasyAdminSubscriber.php
+++ src/EventSubscriber/EasyAdminSubscriber.php
@@ -3,12 +3,13 @@
 namespace App\EventSubscriber;
 
 use App\Entity\Projet;
-Use App\Entity\User;
+use App\Repository\UserRepository;
 use DateTime;
 use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\String\Slugger\SluggerInterface;
+
 
 class EasyAdminSubscriber implements EventSubscriberInterface
 {
@@ -16,7 +17,7 @@ class EasyAdminSubscriber implements EventSubscriberInterface
     private $user;
     private $security;
 
-    public function _construct(SluggerInterface $slugger, Security $security, User $user)
+    public function __construct(SluggerInterface $slugger, Security $security, UserRepository $user)
     {
         $this->slugger = $slugger;
         $this->security = $security;
@@ -35,25 +36,11 @@ class EasyAdminSubscriber implements EventSubscriberInterface
         $entity = $event->getEntityInstance();
 
         if (($entity instanceof Projet)) {
-
             $now = new DateTime('now');
             $entity->setCreatedAt($now);
 
-            $user = $this->$this->security->getUser();
+            $user = $this->security->getUser();
             $entity->setUser($user);
-
-
         }
-
-        if (($entity instanceof Blogpost)) {
-
-            $now = new DateTime('now');
-            $entity->setCreatedAt($now);
-
-            $user = $this->$this->security->getUser();
-            $entity->setUser($user);
-
-        }
-        return;
     }
 }
