3f101b6f8d713c6e65dd48a5c528ef3d2aeb7a9a
DaitoCK <kendhalimax@gmail.com>
2022-02-07 22:07:31 +0100

    create form + envoie à la vue home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,13 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
 use App\Entity\Projet;
+use App\Form\ContactType;
 use App\Repository\CategorieRepository;
 use App\Repository\ProjetRepository;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends AbstractController
 {
@@ -31,11 +34,22 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(ProjetRepository $projetRepository): Response
+    public function index(ProjetRepository $projetRepository, Request $request): Response
     {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contact = $form->getData();
+
+        }
+
         return $this->render('home/index.html.twig', [
             'projets' => $this->projetRepository->findAll(),
+            'form' => $form->createView()
         ]);
+
     }
 
     /**
@@ -47,4 +61,5 @@ class HomeController extends AbstractController
             'projet' => $projet
         ]);
     }
+
 }
